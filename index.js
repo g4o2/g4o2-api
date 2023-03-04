f07e47562a52ea16009bfb46424fcffa200a0544
Hu Kaixiang <maxhu787@gmail.com>
2023-03-04 21:58:41 +0800

    g4o2 api major update

--- index.js
+++ index.js
@@ -2,59 +2,46 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const mysql = require('mysql')
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+const fs = require('fs');
+
 /*
-var con = mysql.createConnection({
-    host: 'sql12.freemysqlhosting.net',
-    user: 'sql12561191',
-    database: 'sql12561191',
-    password: process.env.DB_PASS
-});
-*/
-let con = mysql.createConnection({
-    host: 'localhost',
-    user: 'g4o2',
-    database: 'sql12561191',
-    password: 'g4o2'
-});
 app.get('/', (req, res) => {
     data = { "msg": "hello" };
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify(data, null, 3));
 
+});*/
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 });
-/*
-app.get('/message/:messageId', (req, res) => {
-    let messageId = req.params.messageId;
-    var sql = 'SELECT account, user_id, message FROM chatlog WHERE message_id=?';
-    con.query(sql, [messageId], function (err, result) {
-        if (err) throw err;
-        data = { 
-            "message_id": messageId,
-            "account": result[0]['account'],
-            "user_id": result[0]['user_id'],
-            "message": result[0]['message']
-        };
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify(data, null, 3));
-     });
-});
-*/
-app.get('/message', (req, res) => {
-    let messageId = req.query.id;
-    var sql = 'SELECT account, user_id, message FROM chatlog WHERE message_id=?';
-    con.query(sql, [messageId], function (err, result) {
-        if (err) throw err;
-        data = {
-            "message_id": messageId,
-            "account": result[0]['account'],
-            "user_id": result[0]['user_id'],
-            "message": result[0]['message']
-        };
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify(data, null, 3));
+
+io.on('connection', (socket) => {
+    socket.on('user-connect', (username) => {
+        console.log(`user ${username} connected`);
+        io.emit('user-connect', username);
+    })
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
     });
-});
+    socket.on('message-submit', (messageDetails) => {
+        io.emit('message-submit', messageDetails);
+        console.log(messageDetails);
+        let data = JSON.stringify(messageDetails);
+        data = data.concat(",\r\n");
+        fs.appendFile('./chatlog.json', data, err => {
+            if (err) {
+                console.error(err);
+            }
+        });
+    });
+    socket.on('load-messages', (username) => {
+        let rows = "load chat";
+        io.emit('load-messages', rows);            
+    })
+})
 server.listen(3000, () => {
     console.log('listening on *:3000');
 });
