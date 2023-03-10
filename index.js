057c4be3a99de6a74223d9930602595a55b898c1
Hu Kaixiang <maxhu787@gmail.com>
2023-03-10 21:29:50 +0800

    update

--- index.js
+++ index.js
@@ -7,25 +7,25 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:*', 'https://php-sql-chat.maxhu787.repl.co:*']
+        origin: ['http://localhost:*', 'https://php-sql-chat.maxhu787.repl.co:*', 'https://localhost:*']
     }
 });
 //mysql connection
-/*
+
     let con = mysql.createConnection({
     host: 'localhost',
     user: 'g4o2',
     database: 'sql12561191',
     password: 'g4o2'
 });
-*/
+/*
 var con = mysql.createConnection({
     host: 'sql12.freemysqlhosting.net',
     user: 'sql12561191',
     database: 'sql12561191',
     password: process.env.DB_PASS
 });
-
+*/
 /*
 app.use('/\*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*")
@@ -38,8 +38,13 @@ app.use('/\*', function (req, res, next) {
 //express api
 app.get('/', (req, res) => {
     data = [
-        { "message": "Welcome to g4o2-chat socket.io api" },
-        { "message": "go to /messages to see the chatlog"}
+        { "message": "Welcome to the g4o2-chat api and socket.io server" },
+        { "directories": [
+                "/messages",
+                "/db/users",
+                "/db/messages"
+            ]
+        }
     ];
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify(data, null, 3));
@@ -59,24 +64,36 @@ app.get('/messages', (req, res) => {
     });
 });
 
+app.get('/db', (req, res) => {
+    data = [
+        {"directories": [
+                "/db/users",
+                "/db/messages"
+            ]
+        }
+    ]
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(data, null, 3));
+})
+
 app.get('/db/messages', (req, res) => {
     let message_id = req.query.id;
     if(!message_id) {
         data = {
-            "error": "id parameter not set (example: /db/messages?id=1)"
+            "usage": "/db/messages?id=(message_id))"
         }
         res.setHeader('Content-Type', 'application/json');
         res.send(JSON.stringify(data, null, 3));
     } else {
         var sql = 'SELECT * FROM chatlog WHERE message_id=?';
-        con.query(sql, [message_id], function (err, result) {
+        con.query(sql, [message_id], function (err, responce) {
             if (err) throw err;
             data = {
-                "message_id": result[0]['message_id'],
-                "message": result[0]['message'],
-                "message_date": result[0]['message_date'],
-                "account": result[0]['account'],
-                "user_id": result[0]['user_id']
+                "message_id": responce[0]['message_id'],
+                "message": responce[0]['message'],
+                "message_date": responce[0]['message_date'],
+                "account": responce[0]['account'],
+                "user_id": responce[0]['user_id']
             };
             res.setHeader('Content-Type', 'application/json');
             res.send(JSON.stringify(data, null, 3));
@@ -95,25 +112,45 @@ app.get('/db/users', (req, res) => {
 app.get('/db/users/:userId', (req, res) => {
     let user_id = req.params.userId;
     var sql = 'SELECT * FROM account WHERE user_id=?';
-    con.query(sql, [user_id], function (err, result) {
+    con.query(sql, [user_id], function (err, responce) {
         if (err) console.log(error);
         let email;
-        if (result[0]['show_email'] !== "True") {
+        if (responce[0]['show_email'] !== "True") {
             email = "Hidden";
         } else {
-            email = result[0]['email']
+            email = responce[0]['email']
         }
         data = {
-            "user_id": result[0]['user_id'],
-            "username": result[0]['username'],
-            "name": result[0]['name'],
+            "user_id": responce[0]['user_id'],
+            "username": responce[0]['username'],
+            "name": responce[0]['name'],
             "email": email,
-            "about": result[0]['about']
+            "about": responce[0]['about']
         };
         res.setHeader('Content-Type', 'application/json');
         res.send(JSON.stringify(data, null, 3));
     });
 });
+
+app.get('/db/test', (req, res) => {
+    var sql = 'SELECT * FROM chatlog';
+    con.query(sql, function (err, responce) {
+        if (err) console.log(error);
+        // data = {
+        //     "user_id": responce[0]['user_id'],
+        //     "username": responce[0]['username'],
+        //     "name": responce[0]['name'],
+        //     "email": email,
+        //     "about": responce[0]['about']
+        // };
+        data = {
+            responce
+        };
+        res.setHeader('Content-Type', 'application/json');
+        res.send(JSON.stringify(data, null, 3));
+    });
+});
+
 //socket.io server
 io.on('connection', (socket) => {
     socket.on('user-connect', (username) => {
