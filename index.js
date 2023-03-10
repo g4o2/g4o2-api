f22f2f59ef0efda023d3b9e82afabd52f3c4a83a
Hu Kaixiang <maxhu787@gmail.com>
2023-03-10 21:32:19 +0800

    d

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
     cors: {
-        origin: ['http://localhost:*', 'https://php-sql-chat.maxhu787.repl.co:*', 'https://localhost:*']
+        origin: ['http://localhost/', 'https://php-sql-chat.maxhu787.repl.co:*', 'http://localhost:*']
     }
 });
 //mysql connection
